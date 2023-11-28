89f03aa54a7f90028d578642f58e80cf684530ce
Dario Lopez <dlopez@shutterstock.com>
2023-11-28 15:47:08 +0100

    theme added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { TodoItemMemo as TodoItem } from "./components/TodoItem";
 import { Form } from "./components/Form";
 import { Box, Container, Divider, Heading, Text } from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./components/ColorModeSwitcher";
 
 const initialList = [
   { id: "0", value: "orange", checked: false },
@@ -47,12 +48,34 @@ function App() {
   };
 
   return (
-    <Container marginTop='10rem'>
-      <Heading pb='1rem'>My ToDo</Heading>
-      <Form setTodo={setTodo} />
-      {uncheckedList.length ? (
+    <>
+      <Box textAlign="right">
+        <ColorModeSwitcher />
+      </Box>
+      <Container marginTop="10rem">
+        <Heading pb="1rem">My ToDo</Heading>
+        <Form setTodo={setTodo} />
+        {uncheckedList.length ? (
+          <>
+            {uncheckedList.map((element) => (
+              <TodoItem
+                key={element.id}
+                element={element}
+                changeHandler={changeHandler}
+                clickRemoveHandler={clickRemoveHandler}
+                editInput={editInput}
+              />
+            ))}
+          </>
+        ) : (
+          <Text>Nothing to do!</Text>
+        )}
+        <Box p='1rem'>
+          <Divider />
+        </Box>
+        <Heading size='md'>{checkedList.length} completed item</Heading>
         <>
-          {uncheckedList.map((element) => (
+          {checkedList.map((element) => (
             <TodoItem
               key={element.id}
               element={element}
@@ -62,25 +85,8 @@ function App() {
             />
           ))}
         </>
-      ) : (
-        <Text>Nothing to do!</Text>
-      )}
-      <Box p='1rem'>
-        <Divider />
-      </Box>
-      <Heading size='md'>{checkedList.length} completed item</Heading>
-      <>
-        {checkedList.map((element) => (
-          <TodoItem
-            key={element.id}
-            element={element}
-            changeHandler={changeHandler}
-            clickRemoveHandler={clickRemoveHandler}
-            editInput={editInput}
-          />
-        ))}
-      </>
-    </Container>
+      </Container>
+    </>
   );
 }
 
