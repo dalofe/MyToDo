20d213c389e0857973e020c5abf251bbcd3b98fc
Dario Lopez <dlopez@shutterstock.com>
2023-12-11 18:38:36 +0100

    state nested array OK

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import { useEffect, useState } from "react";
 import { TodoItemMemo as TodoItem } from "./components/TodoItem";
 import { Form } from "./components/Form";
-import { Box, Button, Container, Divider, Heading, Tab, TabList, TabPanel, TabPanels, Tabs, Text, VStack } from "@chakra-ui/react";
+import { Box, Button, Container, Divider, HStack, Heading, Text, VStack } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./components/ColorModeSwitcher";
 
 function App() {
   const [todo, setTodo] = useState(JSON.parse(localStorage.getItem('todos')) || []);
-
-  const checkedList = todo.filter((element) => element.checked);
-  const uncheckedList = todo.filter((element) => !element.checked);
+  const [activeTodo, setActiveTodo] = useState(0);
+  const checkedList = (todo.length) ? todo[activeTodo].filter((element) => element.checked) : [];
+  const uncheckedList = (todo.length) ? todo[activeTodo].filter((element) => !element.checked) : [];
 
   useEffect(() => {
     localStorage.setItem('todos', JSON.stringify(todo));
   }, [todo]);
 
   const changeHandler = (itemCheck, id) => {
-    const updatedTodo = todo.map((element) => {
+    const updatedTodo = todo[activeTodo].map((element) => {
       if (element.id === id) {
         return { ...element, checked: itemCheck };
       } else {
@@ -23,11 +23,27 @@ function App() {
       }
     });
 
-    setTodo(updatedTodo);
+    const updatedList = todo.map((list, index) => {
+      if(index === activeTodo){
+        return updatedTodo;
+      } else {
+        return list;
+      }
+    })
+
+    setTodo(updatedList);
   };
 
   const clickRemoveHandler = (id) => {
-    setTodo(todo.filter((element) => element.id !== id));
+    const updatedTodo = todo[activeTodo].filter(element => element.id !== id);
+    const updatedList = todo.map((list, index) => {
+      if(index === activeTodo){
+        return updatedTodo;
+      } else {
+        return list;
+      }
+    })
+    setTodo(updatedList);
   };
 
   const editInput = (id, updatedValue) => {
@@ -42,6 +58,11 @@ function App() {
     setTodo(updatedTodo);
   };
 
+  const addTodoHandler = () => {
+    setTodo(prevTodo => [...prevTodo, new Array()]);
+    setActiveTodo(prevActiveTodo => prevActiveTodo + 1);
+  }
+
   return (
     <VStack height="100%">
       <Box textAlign="right" width="100%">
@@ -49,7 +70,7 @@ function App() {
       </Box>
       <Container marginTop={["5rem", "10rem"]}>
         <Heading pb="1rem">My ToDo</Heading>
-        <Form setTodo={setTodo} />
+        <Form setTodo={setTodo} activeTodo={activeTodo} arrayTodo={todo} />
         {uncheckedList.length ? (
           <>
             {uncheckedList.map((element) => (
@@ -81,17 +102,14 @@ function App() {
           ))}
         </>
       </Container>
-      <Tabs variant='soft-rounded' width="100%" mt="auto" p="1rem">
-        <TabPanels>
-          <TabPanel>
-            <p>one!</p>
-          </TabPanel>
-        </TabPanels>
-        <TabList>
-          <Button>+</Button>
-          <Tab>One</Tab>
-        </TabList>
-      </Tabs>
+      <HStack width="100%" mt="auto" p="1rem">
+        <Button onClick={addTodoHandler}>+</Button>
+        {todo.map((element, index) => {
+          let colorSchemeValue = "gray";
+          if(index === activeTodo) colorSchemeValue = "yellow";
+          return <Button key={index} colorScheme={colorSchemeValue} onClick={() => setActiveTodo(index)}>{index}</Button>
+        })}
+      </HStack>
     </VStack>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,7 @@
 import { Input, InputGroup, InputRightElement, Button } from "@chakra-ui/react";
 import { useState, useRef } from "react";
 
-export const Form = ({setTodo}) => {
+export const Form = ({setTodo, activeTodo, arrayTodo}) => {
   const [inputText, setInputText] = useState("");
   const inputRef = useRef(null);
 
@@ -14,11 +14,21 @@ export const Form = ({setTodo}) => {
     const input = e.target.querySelector("input");
     const inputValue = input?.value || "";
     const newId = crypto.randomUUID();
+    let todoUpdated = [];
 
-    setTodo((prevTodo) => [
-      ...prevTodo,
-      {id: newId, value: inputValue, checked: false},
-    ]);
+    if(arrayTodo.length) {
+      todoUpdated = arrayTodo.map((todoList, index) => {
+        if(index === activeTodo){
+          return [...todoList, {id: newId, value: inputValue, checked: false}]
+        } else {
+          return todoList;
+        }
+      });
+    } else {
+      todoUpdated.push(new Array({id: newId, value: inputValue, checked: false}));
+    }
+    
+    setTodo(todoUpdated);
     setInputText("");
     inputRef.current.focus();
   };
